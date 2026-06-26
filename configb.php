92b1c2247e4aef9c3f0ccb659a730abd0e0eb55a
Elolo-GIT <enodig.laurent@yahoo.com>
2026-06-26 15:53:38 +0200

    first commit

---
+++ configb.php
@@ -0,0 +1,8 @@
+<?php
+define("ROLE_USER", 2);
+define("ROLE_AVIS", "avis");
+define("ROLE_RAPPORT", "rapport");
+define("ROLE_ADMIN", 1);
+define("ADMIN_ITEM_PER_PAGE", 10);
+define("HABITATS_IMAGES_FOLDER", "./uploads/habitats/");
+define("ASSETS_IMAGES_FOLDER", "/assets/images/");
